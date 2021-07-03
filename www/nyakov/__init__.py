f80e27c5ec247bdbc2c73f521c29087a4de2c80d
Jeremias Pettinen <hi@jeremi.as>
2021-07-03 06:37:32 +0200

    Add UI for specifying Twitch username

--- www/nyakov/__init__.py
+++ www/nyakov/__init__.py
@@ -11,14 +11,18 @@ app = Flask(__name__)
 
 @app.get("/")
 def index():
-    os.chdir("/home/nyakov/nyakov/nyakov")
-    cmdline = ["./build/nyakov", "chatlogs"]
+    user = request.args.get("user")
     try:
-        if re.fullmatch(r"\w+", request.query_string.decode(), re.ASCII):
-            cmdline.append(request.query_string)
+        if not user or not re.fullmatch(r"\w+", user, re.ASCII):
+            user = None
     except UnicodeDecodeError:
         pass
 
+    cmdline = ["./build/nyakov", "chatlogs"]
+    if user:
+        cmdline.append(user)
+
+    os.chdir("/home/nyakov/nyakov/nyakov")
     process = subprocess.run(
         cmdline,
         capture_output=True,
@@ -31,6 +35,11 @@ def index():
     else:
         text = "Unexpected error."
 
+    user_input = '<input type="text" id="user" name="user" placeholder="Twitch user (optional)"'
+    if user:
+        user_input += f' value="{user}"'
+    user_input += ">"
+
     return fr"""<!DOCTYPE html>
 <html>
   <head>
@@ -39,26 +48,38 @@ def index():
     <link rel="icon" href="/favicon.png" type="image/png">
     <style>
       body {{
+        background: black;
+        color: white;
         font-family: sans-serif;
+      }}
+      a {{
+        color: skyblue;
+        text-decoration: none;
       }}
       .bottom {{
         position: absolute;
         bottom: 0;
+        font-size: smaller;
       }}
     </style>
   </head>
   <body>
     <p id="text">{html.escape(text)}</p>
-    <button id="copy">Copy</button>
-    <button id="copy-discord">Copy (Discord-safe)</button>
-    <button id="reload">New quote</button>
-    <p class="bottom"><a href="https://github.com/pettinen/nyakov">Code</a></p>
+    <p>
+      <button id="copy">Copy</button>
+      <button id="copy-discord">Copy (Discord-safe)</button>
+    </p>
+    <form id="new-form" action="/">
+      {user_input}
+      <button id="new-button">New quote</button>
+    </form>
+    <p class="bottom">Generates nonsense from Twitch chatlogs with <a href="https://en.wikipedia.org/wiki/Markov_chain">Markov chains</a>. Code available on <a href="https://github.com/pettinen/nyakov">GitHub</a>.</p>
   <script>
     const copyListener = function(discordSafe) {{
       return async () => {{
         let text = document.getElementById("text").textContent;
         if (discordSafe)
-          text = text.replace(/[\\:@_*~`]/gu, "\\$&");
+          text = text.replace(/[\\/|:_*~`]/gu, "\\$&");
         await navigator.clipboard.writeText(text);
       }};
     }};
@@ -66,12 +87,13 @@ def index():
     document.getElementById("copy").addEventListener("click", copyListener(false));
     document.getElementById("copy-discord").addEventListener("click", copyListener(true));
 
-    const reloadListener = event => {{
-      event.target.textContent = "Loading\u2026";
-      event.target.removeEventListener("click", reloadListener);
-      location.reload();
-    }};
-    document.getElementById("reload").addEventListener("click", reloadListener);
+    document.getElementById("new-form").addEventListener("submit", event => {{
+      document.getElementById("new-button").textContent = "Loading\u2026";
+      if (!document.getElementById("user").value) {{
+        event.preventDefault();
+        location.href = "https://nyakov.aho.ge/";
+      }}
+    }});
   </script>
   </body>
 </html>
